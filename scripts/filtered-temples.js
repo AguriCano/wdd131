962423ef3c06a3ec1a9c9e9fb1f2c3f5ce060a5c
AguriCano <aguricano@gmail.com>
2025-03-31 11:09:43 -0500

    changes_temples

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -1,17 +1,16 @@
-
-// Header hambuger button secction
 document.addEventListener('DOMContentLoaded', () => {
     const hamButton = document.querySelector('#menu');
     const navigation = document.querySelector('.navigation');
     const nameHeader = document.querySelector('.nameHeader');
+    const container = document.querySelector('.container.home');
+    const navLinks = document.querySelectorAll('.navigation a');
+    const titleName = document.querySelector('.title');
 
     hamButton.addEventListener('click', () => {
         navigation.classList.toggle('open');
         hamButton.classList.toggle('open');
         nameHeader.classList.toggle('hidden');
     });
-});
-
 const temples = [
     {
         templeName: "Aba Nigeria",
@@ -19,7 +18,7 @@ const temples = [
         dedicated: "2005, August, 7",
         area: 11500,
         imageUrl:
-            "https://churchofjesuschristtemples.org/assets/img/temples/aba-nigeria-temple/aba-nigeria-temple-5087.jpg",
+            "images/temple1.webp",
         width: "400px",
         height: "250px"
     },
@@ -29,7 +28,7 @@ const temples = [
         dedicated: "2019, dec, 15",
         area: 26969,
         imageUrl:
-            "https://churchofjesuschristtemples.org/assets/img/temples/arequipa-peru-temple/arequipa-peru-temple-7186-main.jpg",
+            "images/temple2.webp",
         width: "400px",
         height: "250px"
     },
@@ -39,7 +38,7 @@ const temples = [
         dedicated: "2024, Jan, 14",
         area: 47413,
         imageUrl:
-            "https://churchofjesuschristtemples.org/assets/img/temples/lima-peru-los-olivos-temple/lima-peru-los-olivos-temple-42524-main.jpg",
+            "images/temple3.webp",
         width: "400px",
         height: "225px"
     },
@@ -49,7 +48,7 @@ const temples = [
         dedicated: "2020, May, 2",
         area: 6861,
         imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg",
+            "images/temple4.webp",
         width: "400px",
         height: "250px"
     },
@@ -59,7 +58,7 @@ const temples = [
         dedicated: "1974, November, 19",
         area: 156558,
         imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg",
+            "images/temple5.webp",
         width: "400px",
         height: "250px"
     },
@@ -69,7 +68,7 @@ const temples = [
         dedicated: "1986, January, 10",
         area: 9600,
         imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg",
+            "images/temple6.webp",
         width: "400px",
         height: "250px"
     },
@@ -79,7 +78,7 @@ const temples = [
         dedicated: "1983, December, 2",
         area: 116642,
         imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg",
+            "images/temple7.webp",
         width: "400px",
         height: "250px"
     },
@@ -89,7 +88,7 @@ const temples = [
         dedicated: "2008, August, 10",
         area: 18943,
         imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/panama-city-panama/400x250/panama-city-temple-lds-569186-wallpaper.jpg",
+            "images/temple8.webp",
         width: "400px",
         height: "250px"
     },
@@ -99,7 +98,7 @@ const temples = [
         dedicated: "2000, June, 11",
         area: 10700,
         imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/fukuoka-japan/400x250/fukuoka-japan-temple-lds-306863-wallpaper.jpg",
+            "images/temple9.webp",
         width: "400px",
         height: "250px"
     },
@@ -109,7 +108,7 @@ const temples = [
         dedicated: "2017, May, 21",
         area: 44175,
         imageUrl:
-            "https://churchofjesuschristtemples.org/assets/img/temples/paris-france-temple/paris-france-temple-2056-main.jpg",
+            "images/temple10.webp",
         width: "400px",
         height: "225px"
     },
@@ -119,7 +118,7 @@ const temples = [
         dedicated: "2006, February, 27",
         area: 16350,
         imageUrl:
-            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/helsinki-finland/400x225/helsinki-finland-temple-lds-354498-wallpaper.jpg",
+            "images/temple11.webp",
         width: "400px",
         height: "225px"
     },
@@ -129,82 +128,77 @@ const temples = [
         dedicated: "2022, November, 20",
         area: 28675,
         imageUrl:
-            "https://churchofjesuschristtemples.org/assets/img/temples/belem-brazil-temple/belem-brazil-temple-31310-main.jpg",
+            "images/temple12.webp",
         width: "400px",
         height: "225px"
     },
 
 ];
 
-document.addEventListener('DOMContentLoaded', () => {
-    const container = document.querySelector('.container.home');
-    const navLinks = document.querySelectorAll('.navigation a');
-    const titleName = document.querySelector('.title');
-
-    const displayTemples = (filteredTemples) => {
-        container.innerHTML = '';
-        filteredTemples.forEach(temple => {
-            const card = document.createElement('div');
-            card.className = 'temple-card';
-            card.innerHTML = `
-                <img src="${temple.imageUrl}"  loading="lazy"  width="${temple.width}" height="${temple.height}" alt="${temple.templeName} Temple">
-                <div class="temple-info">
-                    <h2>${temple.templeName}</h2>
-                    <p><strong>Location:</strong> ${temple.location}</p>
-                    <p><strong>Dedicated:</strong> ${temple.dedicated}</p>
-                    <p><strong>Area:</strong> ${temple.area.toLocaleString()} sq ft</p>
-                </div>
-            `;
-            container.appendChild(card);
-        });
-    };
-
-    const filterTemples = (criteria) => {
-        let filteredTemples = temples;
-        switch (criteria) {
-            case 'old':
-                filteredTemples = temples.filter(temple => new Date(temple.dedicated).getFullYear() < 1900);
-                titleName.innerHTML = 'Old Temples';
-                break;
-            case 'new':
-                filteredTemples = temples.filter(temple => new Date(temple.dedicated).getFullYear() > 2000);
-                titleName.innerHTML = 'New Temples';
-                break;
-            case 'large':
-                filteredTemples = temples.filter(temple => temple.area > 90000);
-                titleName.innerHTML = 'Large Temples';
-                break;
-            case 'small':
-                filteredTemples = temples.filter(temple => temple.area < 10000);
-                titleName.innerHTML = 'Small Temples';
-                break;
-            default:
-                filteredTemples = temples;
-                titleName.innerHTML = 'All Temples';
-                break;
-        }
-        displayTemples(filteredTemples);
-    };
-
-    navLinks.forEach(link => {
-        link.addEventListener('click', (e) => {
-            e.preventDefault();
-            navLinks.forEach(nav => nav.classList.remove('active'));
-            e.target.classList.add('active');
-            filterTemples(e.target.id);
-        });
+// SHOW TEMPLES
+const displayTemples = (filteredTemples) => {
+    container.innerHTML = '';
+    filteredTemples.forEach(temple => {
+        const card = document.createElement('div');
+        card.className = 'temple-card';
+        card.innerHTML = `
+            <img src="${temple.imageUrl}" loading="lazy" width="${temple.width}" height="${temple.height}" alt="${temple.templeName} Temple">
+            <div class="temple-info">
+                <h2>${temple.templeName}</h2>
+                <p><strong>Location:</strong> ${temple.location}</p>
+                <p><strong>Dedicated:</strong> ${temple.dedicated}</p>
+                <p><strong>Area:</strong> ${temple.area.toLocaleString()} sq ft</p>
+            </div>
+        `;
+        container.appendChild(card);
     });
+};
 
-    filterTemples('home-nav');
+// FILTERED TEMPLES
+const filterTemples = (criteria) => {
+    let filteredTemples = temples;
+    switch (criteria) {
+        case 'old':
+            filteredTemples = temples.filter(temple => new Date(temple.dedicated).getFullYear() < 1900);
+            titleName.innerHTML = 'Old Temples';
+            break;
+        case 'new':
+            filteredTemples = temples.filter(temple => new Date(temple.dedicated).getFullYear() > 2000);
+            titleName.innerHTML = 'New Temples';
+            break;
+        case 'large':
+            filteredTemples = temples.filter(temple => temple.area > 90000);
+            titleName.innerHTML = 'Large Temples';
+            break;
+        case 'small':
+            filteredTemples = temples.filter(temple => temple.area < 10000);
+            titleName.innerHTML = 'Small Temples';
+            break;
+        default:
+            filteredTemples = temples;
+            titleName.innerHTML = 'All Temples';
+            break;
+    }
+    displayTemples(filteredTemples);
+};
+
+// ADD EVENTS
+navLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        navLinks.forEach(nav => nav.classList.remove('active'));
+        e.target.classList.add('active');
+        filterTemples(e.target.id);
+    });
 });
 
 
-// Footer information
-document.addEventListener('DOMContentLoaded', () => {
-    const currentYear = new Date().getFullYear();
-    const lastModified = document.lastModified;
-    const copyrightYearElement = document.getElementById('currentyear');
-    const lastModifiedElement = document.getElementById('lastModified');
-    copyrightYearElement.textContent = currentYear;
-    lastModifiedElement.textContent = `Last update: ${lastModified}`;
+filterTemples('home-nav');
+
+const currentYear = new Date().getFullYear();
+const lastModified = document.lastModified;
+const copyrightYearElement = document.getElementById('currentyear');
+const lastModifiedElement = document.getElementById('lastModified');
+copyrightYearElement.textContent = currentYear;
+lastModifiedElement.textContent = `Last update: ${lastModified}`;
 });
